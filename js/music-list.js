b1a0cfb90c926f28dc5a07786f042378720cea61
Abraham Muller <fikiriabramsg@gmail.com>
2022-07-12 11:38:15 +0200

    Mini Player-music
    Done in Javacsript

---
+++ js/music-list.js
@@ -0,0 +1,57 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+//    This app made by ABRAHAM MULLER
+
+let allMusic = [
+  {
+    name: "Polaroid experience",
+    artist: "Youssoupha",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Memento",
+    artist: "Youssoupha",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Entourage",
+    artist: "Youssoupha",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Negretude",
+    artist: "Youssoupha",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Le jour ou j ai arrete le rap",
+    artist: "Youssoupha",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Mourir ensemble",
+    artist: "Youssoupha",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
